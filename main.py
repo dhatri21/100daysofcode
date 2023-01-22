0890d8e820845142aaeb824475e239b08c07e5ec
Dhatri Acharya <112926596+dhatri21@users.noreply.github.com>
2023-01-22 18:15:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
-#1. Create a greeting for your program.
 
-#2. Ask the user for the city that they grew up in.
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $\n"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15? \n"))
+people = int(input("How many people to split the bill?\n"))
 
-#3. Ask the user for the name of a pet.
-
-#4. Combine the name of their city and pet and show them their band name.
-
-#5. Make sure the input cursor shows on a new line:
-
-# Solution: https://replit.com/@appbrewery/band-name-generator-end
-
-
-print("Need a super name for your super band? You are at a right place!")
-
-city = input("Which city are you grew up in? ")
-
-pet = input("What is the name of your pet? ")
-
-print("Your band name is " + city + " " + pet + "!")
-
+tip_as_percent = tip / 100
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
+final_amount = round(bill_per_person, 2)
+print(f"Each person should pay: ${final_amount}")
