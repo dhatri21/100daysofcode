1d42e6414ea2d3c2eedc1058f437d5710e24affa
Dhatri Acharya <112926596+dhatri21@users.noreply.github.com>
2023-01-24 19:15:19 +0530

    Update main.py
    day 03 project - treasure island

--- main.py
+++ main.py
@@ -1,21 +1,44 @@
-#1. Create a greeting for your program.
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
 
-#2. Ask the user for the city that they grew up in.
+left_right = (input("Do you want to go left or right? \n")).lower()
 
-#3. Ask the user for the name of a pet.
-
-#4. Combine the name of their city and pet and show them their band name.
-
-#5. Make sure the input cursor shows on a new line:
-
-# Solution: https://replit.com/@appbrewery/band-name-generator-end
-
-
-print("Need a super name for your super band? You are at a right place!")
-
-city = input("Which city are you grew up in? ")
-
-pet = input("What is the name of your pet? ")
-
-print("Your band name is " + city + " " + pet + "!")
-
+if left_right == "left":
+  swim_wait = input("Do you want to swim or wait? \n").lower()
+  if swim_wait == "wait":
+    which_door = input("Which door would you choose? Blue, yellow or red? \n").lower()
+    if which_door == "red":
+      print("Burned by fire. Game over!")
+    elif which_door == "blue":
+      print("Eaten by beast. Game over!")
+    elif which_door == "yellow":
+      print("You win!")
+    else:
+      print("Game over!")
+  else:
+    print("Attacked by trout. Game over!")
+else:
+  print("Fall into a hole. Game over!")
+     
