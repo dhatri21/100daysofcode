7ad64b115fd996369507bfda75e9a027cf34b978
Dhatri Acharya <112926596+dhatri21@users.noreply.github.com>
2023-02-09 14:55:57 +0530

    Password generator
    create strong password

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
-#1. Create a greeting for your program.
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-#2. Ask the user for the city that they grew up in.
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input("How many symbols would you like?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
 
-#3. Ask the user for the name of a pet.
+password = []
+passwords = ""
 
-#4. Combine the name of their city and pet and show them their band name.
+for nr_letters in range(1,nr_letters+1):
+  random_letter = random.randint(1,len(letters))
+  password.append(letters[random_letter])
 
-#5. Make sure the input cursor shows on a new line:
+for nr_symbols in range(1,nr_symbols+1):
+  random_symbols = random.randint(0,len(symbols)-1)
+  password.append(symbols[random_symbols])
 
-# Solution: https://replit.com/@appbrewery/band-name-generator-end
+for nr_numbers in range(1,nr_numbers+1):
+  random_numbers = random.randint(0,len(numbers)-1)
+  print(random_numbers)
+  password.append(numbers[random_numbers])
+  
+for n in range(0,len(password)):
+  passwords += password[n]
 
-
-print("Need a super name for your super band? You are at a right place!")
-
-city = input("Which city are you grew up in? ")
-
-pet = input("What is the name of your pet? ")
-
-print("Your band name is " + city + " " + pet + "!")
-
+print(passwords)
